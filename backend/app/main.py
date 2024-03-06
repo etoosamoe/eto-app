cba74cba00a63f2859815930d034a3e5d6c5b17c
Yuriy Semyenkov <yuriy.semyenkov@gmail.com>
2024-03-06 08:37:13 +0100

    Add Sentry conditional using

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,16 +17,14 @@ from .database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 
-sentry_sdk.init(
-    dsn = os.environ.get("SENTRY_DSN"),
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    traces_sample_rate=1.0,
-    # Set profiles_sample_rate to 1.0 to profile 100%
-    # of sampled transactions.
-    # We recommend adjusting this value in production.
-    profiles_sample_rate=1.0,
-)
+sentry_dsn = os.environ.get("SENTRY_DSN")
+if sentry_dsn:
+    sentry_sdk.init(
+        dsn=sentry_dsn,
+        traces_sample_rate=1.0,
+        profiles_sample_rate=1.0,
+    )
+    
 app = FastAPI()
 
 app.add_middleware(
